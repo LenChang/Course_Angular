6ae19c3ce6fb9da9bf9cc8a8d1e318331d29ac94
Len Chang <lamgo.chang@gmail.com>
2017-10-30 18:12:28 +0800

    - Add example: Property binding

--- my-dream-app/src/app/servers/servers.component.ts
+++ my-dream-app/src/app/servers/servers.component.ts
@@ -1,19 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector:'app-servers', // element
+  selector: 'app-servers', // element
   // selector: '[app-servers]', // parameter
   // selector: '.app-servers',  // class
-  templateUrl: './servers.component.html',
   //--- inline code ---//
   // template: `
   //   <app-server></app-server>
-  //   <app-server></app-server>`,
+  //   <app-server></app-server>`
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
