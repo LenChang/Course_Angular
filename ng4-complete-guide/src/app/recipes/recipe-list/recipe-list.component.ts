4637af2e87402402cc49c794618fb1903342989e
Len Chang <lamgo.chang@gmail.com>
2018-02-27 11:48:42 +0800

    - Update samples

--- ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ng4-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,9 +14,9 @@ export class RecipeListComponent implements OnInit {
       "https://c.pxhere.com/photos/9a/90/food_gastronomy_meat_tasty_dinner_restaurant_meal-1376098.jpg!d"
     ),
     new Recipe(
-      "A Test Recipe",
-      "This's a simply a test",
-      "https://c.pxhere.com/photos/9a/90/food_gastronomy_meat_tasty_dinner_restaurant_meal-1376098.jpg!d"
+      "A Test Recipe 2",
+      "This's a simply a test 2",
+      "https://www.popsci.com/sites/popsci.com/files/styles/655_1x_/public/images/2017/11/chocolate_cake.jpg?itok=dqopi_kH&fc=50,50"
     ),
   ];
 
