6ab10a472724f4440af18db1a43793b63dbea046
Len Chang <lamgo.chang@gmail.com>
2018-03-31 21:50:00 +0800

    - Change grid cols depends on screen size

--- Responsive-Angular/src/app/app.component.ts
+++ Responsive-Angular/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ObservableMedia } from "@angular/flex-layout";
+
+// Ref: https://stackoverflow.com/questions/45188134/change-the-layout-or-cols-value-of-md-grid-list-based-on-screen-size
+// Ref: http://brianflove.com/2017/05/03/responsive-angular/
+// Ref: https://material.angular.io/components/categories
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private observableMedia: ObservableMedia) {
+    observableMedia.asObservable().subscribe(change => {
+      if (change.mqAlias == "xs") {
+        this.cols = 1;
+      } else if (change.mqAlias == "sm") {
+        this.cols = 2;
+      } else {
+        this.cols = 3;
+      }
+      // console.log(change.mqAlias);
+    });
+  }
+  title = "app";
+
+  /**
+   * The number of colums in the md-grid-list directive.
+   */
+  cols = 2;
+
+  ngOnInit() {
+    this.cols = 1;
+  }
 }
--- Responsive-Angular/src/app/app.module.ts
+++ Responsive-Angular/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatGridListModule } from "@angular/material/grid-list";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [FlexLayoutModule, BrowserModule],
+  imports: [FlexLayoutModule, BrowserModule, MatGridListModule],
   providers: [],
   bootstrap: [AppComponent],
 })
